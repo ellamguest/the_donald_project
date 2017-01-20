8bbe4789903054265df9cd7cf5cb3c14ad1f5231
ellamguest <ellamguest@gmail.com>
2017-01-20 17:26:40 +0000

    attempting clustering (k-mean & hierarchical) & topic modelling (LDA)

--- scripts/doc_clustering.py
+++ scripts/doc_clustering.py
@@ -16,7 +16,7 @@ import mpld3
 from revisions_df_tools import pull_page, json_to_html
 
 '''working w/ 2 lists:
-times = for labelling (aka titles)
+times or authors = for labelling (aka titles)
 texts = for content (aka synopses)
 '''
 
@@ -29,6 +29,7 @@ sample['json'] = sample['url'].map(pull_page)
 sample['html'] = sample['json'].map(json_to_html)
 
 times = sample.index
+titles = sample['author']
 texts = []
 for text in sample['html']:
     texts.append(text.get_text())
@@ -136,9 +137,9 @@ clusters = km.labels_.tolist()
 
 # create doc attribute df including cluster assignment
 authors = sample['author']
-revs = { 'time': times, 'author': sample['author'].tolist(), 'text': texts, 'cluster': clusters}
+revs = { 'time': times, 'author': sample['author'].tolist(), 'text': texts, 'cluster': clusters, 'month' : sample.index.month}
 
-frame = pd.DataFrame(revs, index = [clusters] , columns = ['time', 'author', 'cluster', 'text'])
+frame = pd.DataFrame(revs, index = [clusters] , columns = ['time', 'author', 'cluster', 'text', 'month'])
 
 frame['cluster'].value_counts()
 
@@ -203,7 +204,7 @@ print()
 # step 1 define some dictionaries for going from cluster number to color and to cluster name
 
 #set up colors per clusters using a dict
-cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e', 5: '#66a61e'}
+cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e', 5: '#3364FF'}
               
 cluster_names = {}
 # cannibalizing code above to get list of highest ranking words per cluster
@@ -218,45 +219,200 @@ for i in range(num_clusters):
 %matplotlib inline 
 
 #create data frame that has the result of the MDS plus the cluster numbers and titles
-df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=times)) 
-
+df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=sample['author'])) 
+  
+df['shapes'] = df['title'].map({1:'o',11:'^',12:'s'}) # want to make shape correspond to month of revision, come back to
 #group by cluster
 groups = df.groupby('label')
 
 
 # set up plot
-fig, ax = plt.subplots(figsize=(17, 9)) # set size
-ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+def plot():
+    fig, ax = plt.subplots(figsize=(11, 6)) # set size
+    ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+    
+    #iterate through groups to layer the plot
+    #note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
+ 
+    for name, group in groups:
+        ax.plot(group.x, group.y, marker='o', linestyle='', ms=12, 
+                label=cluster_names[name], color=cluster_colors[name], 
+                mec='none')
+        ax.set_aspect('auto')
+        ax.tick_params(\
+            axis= 'x',          # changes apply to the x-axis
+            which='both',      # both major and minor ticks are affected
+            bottom='off',      # ticks along the bottom edge are off
+            top='off',         # ticks along the top edge are off
+            labelbottom='off')
+        ax.tick_params(\
+            axis= 'y',         # changes apply to the y-axis
+            which='both',      # both major and minor ticks are affected
+            left='off',      # ticks along the bottom edge are off
+            top='off',         # ticks along the top edge are off
+            labelleft='off')
+        
+    ax.legend(numpoints=1, loc=0)  #show legend with only 1 point
+    
+    #add label in x,y position with the label as the author name
+    for i in range(len(df)):
+        ax.text(df.ix[i]['x'], df.ix[i]['y'], df.ix[i]['title'], size=8)  
+    
+        
+        
+    plt.show() #show the plot
+
+plot()
+plt.close()
+
+#plt.savefig('clusters_small_noaxes.png', dpi=200)
+
+
+''' INTERACTIVE VISUALISATION W/ MPLD3 - beyond me atm
+using mpld3 for D3.js interactive graphic capabilites
+!! need to go over toolbar bit !!
+might want to change is the x and y attr for the position of the toolbar
+
+#define custom toolbar location
+class TopToolbar(mpld3.plugins.PluginBase):
+    """Plugin for moving toolbar to top of figure"""
+
+    JAVASCRIPT = """
+    mpld3.register_plugin("toptoolbar", TopToolbar);
+    TopToolbar.prototype = Object.create(mpld3.Plugin.prototype);
+    TopToolbar.prototype.constructor = TopToolbar;
+    function TopToolbar(fig, props){
+        mpld3.Plugin.call(this, fig, props);
+    };
+
+    TopToolbar.prototype.draw = function(){
+      // the toolbar svg doesn't exist
+      // yet, so first draw it
+      this.fig.toolbar.draw();
+
+      // then change the y position to be
+      // at the top of the figure
+      this.fig.toolbar.toolbar.attr("x", 150);
+      this.fig.toolbar.toolbar.attr("y", 400);
+
+      // then remove the draw function,
+      // so that it is not called again
+      this.fig.toolbar.draw = function() {}
+    }
+    """
+    def __init__(self):
+        self.dict_ = {"type": "toptoolbar"}
+
+#create data frame that has the result of the MDS plus the cluster numbers and titles
+df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=titles)) 
+
+#group by cluster
+groups = df.groupby('label')
+
+#define custom css to format the font and to remove the axis labeling
+css = """
+text.mpld3-text, div.mpld3-tooltip {
+  font-family:Arial, Helvetica, sans-serif;
+}
+
+g.mpld3-xaxis, g.mpld3-yaxis {
+display: none; }
+
+svg.mpld3-figure {
+margin-left: -200px;}
+"""
+
+# Plot 
+fig, ax = plt.subplots(figsize=(14,6)) #set plot size
+ax.margins(0.03) # Optional, just adds 5% padding to the autoscaling
 
 #iterate through groups to layer the plot
-#note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
 for name, group in groups:
-    ax.plot(group.x, group.y, marker='o', linestyle='', ms=12, 
-            label=cluster_names[name], color=cluster_colors[name], 
-            mec='none')
+    points = ax.plot(group.x, group.y, marker='o', linestyle='', ms=18, 
+                     label=cluster_names[name], mec='none', 
+                     color=cluster_colors[name])
     ax.set_aspect('auto')
-    ax.tick_params(\
-        axis= 'x',          # changes apply to the x-axis
-        which='both',      # both major and minor ticks are affected
-        bottom='off',      # ticks along the bottom edge are off
-        top='off',         # ticks along the top edge are off
-        labelbottom='off')
-    ax.tick_params(\
-        axis= 'y',         # changes apply to the y-axis
-        which='both',      # both major and minor ticks are affected
-        left='off',      # ticks along the bottom edge are off
-        top='off',         # ticks along the top edge are off
-        labelleft='off')
+    labels = [i for i in group.title]
     
-ax.legend(numpoints=1)  #show legend with only 1 point
-
-#add label in x,y position with the label as the film title
-for i in range(len(df)):
-    ax.text(df.ix[i]['x'], df.ix[i]['y'], df.ix[i]['title'], size=8)  
+    #set tooltip using points, labels and the already defined 'css'
+    tooltip = mpld3.plugins.PointHTMLTooltip(points[0], labels,
+                                       voffset=10, hoffset=10, css=css)
+    #connect tooltip to fig
+    mpld3.plugins.connect(fig, tooltip, TopToolbar())    
+    
+    #set tick marks as blank
+    ax.axes.get_xaxis().set_ticks([])
+    ax.axes.get_yaxis().set_ticks([])
+    
+    #set axis as blank
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
 
     
-    
-plt.show() #show the plot
+ax.legend(numpoints=1) #show legend with only one dot
 
-#uncomment the below to save the plot if need be
-#plt.savefig('clusters_small_noaxes.png', dpi=200)
+mpld3.display() #show the plot
+
+#uncomment the below to export to html
+html = mpld3.fig_to_html(fig)
+print(html)
+
+'''
+
+'''
+Hierarchical document clustering
+Ward clustering algorithm - agglomerative clustering method
+step 1) used the precomputed cosine distance matrix (dist) to calclate a linkage_matrix
+step 2) then plot as a dendrogram.
+'''
+
+from scipy.cluster.hierarchy import ward, dendrogram
+
+linkage_matrix = ward(dist) #define the linkage_matrix using ward clustering pre-computed distances
+
+fig, ax = plt.subplots(figsize=(5, 7)) # set size
+ax = dendrogram(linkage_matrix, orientation="right", labels=titles);
+
+plt.tick_params(\
+    axis= 'x',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom='off',      # ticks along the bottom edge are off
+    top='off',         # ticks along the top edge are off
+    labelbottom='off')
+
+plt.tight_layout() #show plot with tight layout
+
+#uncomment below to save figure
+#plt.savefig('ward_clusters.png', dpi=200) #save figure as ward_clusters
+plt.close()
+
+'''
+Latent Dirichlet Allocation - topic modelling
+LDA is a probabilistic topic model that assumes documents are a mixture of topics and that each word in the document is attributable to the document's topics
+For my implementaiton of LDA, I use the Gensim pacakage.
+preprocess the synopses a bit differently here - HE REMOVED PROPPER NOUNS, I WILL NOT
+'''
+
+from gensim import corpora, models, similarities 
+
+#tokenize
+tokenized_text = [tokenize_and_stem(text) for text in texts]
+
+#remove stop words
+texts = [[word for word in text if word not in stopwords] for text in tokenized_text]
+
+#create a Gensim dictionary from the texts
+dictionary = corpora.Dictionary(texts)
+
+#remove extremes (similar to the min/max df step used when creating the tf-idf matrix)
+dictionary.filter_extremes(no_below=1, no_above=0.8)
+
+#convert the dictionary to a bag of words corpus for reference
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+# run the model
+%time lda = models.LdaModel(corpus, num_topics=5, 
+                            id2word=dictionary, 
+                            update_every=5, 
+                            chunksize=10000, 
+                            passes=100)
