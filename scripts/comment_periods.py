bec9fd6c8f940da6d2c1f99dfcf5a0ff0d462d23
ellamguest <ellamguest@gmail.com>
2017-02-02 17:22:33 +0000

    compiling top posts across all time period beginnings

--- scripts/comment_periods.py
+++ scripts/comment_periods.py
@@ -7,10 +7,15 @@ Created on Thu Feb  2 14:37:53 2017
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from datetime import datetime
+from datetime import datetime, timedelta
+import requests
+from bs4 import BeautifulSoup
+import re
 
 # use cluster_analysis.py to produce clustering df (cdf)
 
+############## GET DATA
+# use cluster_analysis.py to produce updated clustering df (cdf)
 df = pd.read_csv('/Users/emg/Programmming/GitHub/the_donald_project/raw_data/all_mods_merged.csv', index_col=1)
 cdf = pd.read_csv('/Users/emg/Programmming/GitHub/the_donald_project/tidy_data/time_clusters.csv', index_col=0)
 cdf.index = pd.to_datetime(cdf.index)
@@ -21,23 +26,55 @@ begins.columns = ['begins','cluster']
 ends = cdf.groupby('cluster').tail(1).reset_index()
 ends.columns = ['ends','cluster']
 
+# getting exact time periods
 periods = pd.merge(begins,ends)
-periods = periods[['begins','ends','cluster']]
-
-url = 'https://www.reddit.com/r/help/search?q=timestamp:1458432000..1461456000&sort=top&restrict_sr=on&syntax=cloudsearch'
-
-lowers = []
-for x in periods['begins']:
-    lowers.append(str(x.value)[:-9])
-periods['lower'] = lowers
-
-uppers = []
-for x in periods['ends']:
-    uppers.append(str(x.value)[:-9])
-periods['upper'] = uppers
-
+periods['lower'] = [str(x.value)[:-9] for x in periods['begins']]
+periods['upper'] = [str(x.value)[:-9] for x in periods['ends']]
 periods['range'] = periods['lower'] + '..' + periods['upper']
 
-periods['url'] = periods['range'].map(lambda x:"""https://www.reddit.com/r/the_donald/
-                search?q=timestamp:{}&sort=top&restrict_sr=on&syntax=cloudsearch""".format(x))
+# getting week window around beginning of time period
+l_frame = periods['begins'].map(lambda x: x - timedelta(days=7))
+periods['l_frame'] = [str(x.value)[:-9] for x in l_frame]
+r_frame = periods['begins'].map(lambda x: x + timedelta(days=7))
+periods['r_frame'] = [str(x.value)[:-9] for x in r_frame]
+periods['window'] = periods['l_frame'] + '..' + periods['r_frame']
+periods['url'] = periods['window'].map(lambda x:'https://www.reddit.com/r/the_donald/search?q=timestamp:{}&sort=top&restrict_sr=on&syntax=cloudsearch'.format(x))
+
+################# PULL COMMENTS
+
+
+
+from scraping_functions import *
+
+##### GET ALL ARCHIVED COPIES from archive.it
+
+def make_soup(url):
+    headers = {'user-agent': 'why_ask_reddit 1.0'}
+    r = requests.get(test, headers=headers)
+    soup = BeautifulSoup(r.text, "html5lib")
+    return soup
+
+soup = make_soup(test)
+
+def get_top_posts(soup):
+    '''soup is a BeautifulSoup object'''
+    headers = soup.findAll("header", { "class" : "search-result-header" })
+    titles = [x.a.text for x in headers]
+    tags = soup.findAll("div", { "class" : "search-result-meta" })
+    urls = [x.a['href'] for x in tags]
+    dt = [x.time['datetime'] for x in tags]
+    author = [x.find_all('span', {'class':'search-author'})[0].a['href'] for x in tags]
+    score = [x.find('span', {'class':'search-score'}).text[:-7] for x in tags]
+    start = periods['begins'][0]    
+    top_posts = pd.DataFrame(data = {'title':titles,'date':dt,'url':urls,'author':author,'score':score, 'start':start})
+    return top_posts
+    
+
+
+dfs = []
+for i in periods.index:
+    soup = make_soup(periods['url'][i])
+    df = get_top_posts(soup)
+    dfs.append(df)
+    
 
