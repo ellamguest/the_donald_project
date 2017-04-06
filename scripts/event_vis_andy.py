d07c6dbb487a74fffa3717312a0755ff64d2eda8
Ella Guest <ellamguest@gmail.com>
2017-04-06 15:26:57 +0100

    wrote own script for mod timelines
    adding permission level and qualitative colour palette

--- scripts/event_vis_andy.py
+++ scripts/event_vis_andy.py
@@ -18,38 +18,49 @@ df['pubdate'] = df['pubdate'].astype(str)
 df['pubdate'] = df['pubdate'].apply(lambda t:'{}/{}/{}'.format(t[:4],t[4:6],t[6:8]))
 df['pubdate'] = pd.to_datetime(df['pubdate'])
 
-subset = (df[['name', 'date', 'pubdate']].copy()
-            .assign(
-                date=lambda df: df['date'].pipe(pd.to_datetime).dt.normalize(),
-                pubdate=lambda df: df['pubdate'].pipe(pd.to_datetime).dt.normalize()))
-
-subset = subset.sort_values(['name','date'])
-
-seen = (subset
-                .groupby(['date', 'name']).first()['pubdate']
-                .unstack()
-                .isnull()
-                .pipe(lambda x: ~x)
-                .resample('D').mean()
-                .fillna(0)
-                .astype(bool))
-
-
-not_seen = (subset
-                .groupby(['pubdate', 'name']).first()['date']
-                .unstack()
-                .isnull()
-                .resample('D').pad())
+def weekly_mod_timeline(df):
+    subset = (df[['name', 'date', 'pubdate']].copy()
+                .assign(
+                    date=lambda df: df['date'].pipe(pd.to_datetime).dt.normalize(),
+                    pubdate=lambda df: df['pubdate'].pipe(pd.to_datetime).dt.normalize()))
     
+    subset = subset.sort_values(['name','date'])
+    
+    seen = (subset
+                    .groupby(['date', 'name']).first()['pubdate']
+                    .unstack()
+                    .isnull()
+                    .pipe(lambda x: ~x)
+                    .resample('D').mean()
+                    .fillna(0)
+                    .astype(bool))
+    
+    
+    not_seen = (subset
+                    .groupby(['pubdate', 'name']).first()['date']
+                    .unstack()
+                    .isnull())
+    
+    not_seen_old = (subset_old
+                    .groupby(['pubdate', 'name']).first()['date']
+                    .unstack()
+                    .isnull()
+                    .resample('D').pad())
+        
+    
+    output = {}
+    current = pd.Series(False, seen.columns)    
+    for d in seen.index & not_seen.index:
+        joined, left = seen.loc[d], not_seen.loc[d]
+        current[joined] = True
+        current[left] = False
+        output[d] = current.copy()
+    output = pd.concat(output, 1).T
+    weeks = output.resample('W').last()
+    return weeks
 
-output = {}
-current = pd.Series(False, seen.columns)    
-for d in seen.index & not_seen.index:
-    joined, left = seen.loc[d], not_seen.loc[d]
-    current[joined] = True
-    current[left] = False
-    output[d] = current.copy()
-output = pd.concat(output, 1).T
+weeks = weekly_mod_timeline(df)
+weeks.head()
 
 output.to_csv('/Users/emg/Programmming/GitHub/the_donald_project/tidy_data/day_mod_matrix.csv')
 
@@ -67,4 +78,5 @@ plt.setp(cg.ax_heatmap.yaxis.get_majorticklabels(), rotation=0)
 cg.ax_heatmap.set_title('Timeline of Moderator Presence', fontsize=20)
 cg.ax_heatmap.set_ylabel('Moderator', fontsize=15)
 cg.ax_heatmap.set_xlabel('Date (in weeks)', fontsize=15)
+
 
